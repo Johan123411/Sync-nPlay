37bf47656e90aec483ad38036558a75dc655cfb1
Johan123411 <baruasiddhant123411@gmal.com>
2020-05-01 15:51:59 -0400

    cache constants and index

---
+++ core/cache.js
@@ -0,0 +1,27 @@
+export const cache = {
+	parties: [
+		// {
+		// 	partyId: 0,
+		// 	selectedVideo: {},
+		// 	waitingForAllUsersToBeReady: false,
+		// 	videoPlayer: {
+		// 		playerState: 'paused',
+		// 		timeInVideo: 0,
+		// 		lastStateChangeInitiator: null
+		// 	},
+		// 	videoPlayerInterval: null,
+		// 	usersInParty: [],
+		// 	messagesInParty: []
+		// }
+	],
+	users: [
+		// {
+		// 	socketId: '',
+		// 	userName: '',
+		// 	readyToPlayState: {
+		// 		clientIsReady: false,
+		// 		timeInVideo: 0
+		// 	}
+		// }
+	]
+}
